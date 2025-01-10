e483e18523d2db822133a749a17c250d83b53286
WycliffeBenedict1 <wycliffebenedict24@gmail.com>
2025-01-10 23:47:38 +0300

    function lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    //returns the number of blocks given a value
+    const hq = 42;
+     return Math.abs(blocks - hq);
+  }
+
+  function distanceFromHqInFeet(blocks) {
+    const feetperblock = 264
+     return distanceFromHqInBlocks(blocks) * feetperblock ;
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    const feetPerBlock = 264;
+     return Math.abs(destination - start) * feetPerBlock;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    if (distanceInFeet <=400) {
+        return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <=2000){
+        return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet >2000 && distanceInFeet <=2500) {
+        return 25
+    } else {
+        return "cannot travel that far";
+    }
+
+  }
